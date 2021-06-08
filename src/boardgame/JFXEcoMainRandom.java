9548eb66442e4d6c35ee02f32555139f2546f4a5
Alexandre Ledun <alexandre.ledun.etu@univ-lille.fr>
2021-06-08 16:05:09 +0200

    Séparation du Pane principale (root) en 3 Pane - la Pane qui affiche le plateau comme c'était déjà le cas - une Pane qui servira à afficher les infos de la partie (vainqueur etc?) - Une Pane qui servira à afficher les infos détaillées des joueurs (unités déployées etc?) On peut encore envisager de rajouter une Pane qui n'affiche que les infos de la Cell sur laquelle on hover la souris

--- src/boardgame/JFXEcoMainRandom.java
+++ src/boardgame/JFXEcoMainRandom.java
@@ -16,46 +16,46 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 
-public class Main extends Application {
-	private VBox boardVBox;
-	private Scene boardScene;
+public class JFXEcoMainRandom extends Application {
+	private HBox mainScreenHBox;
+	private Scene mainScreenScene;
 	private Stage primaryStage;
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			this.primaryStage = primaryStage;
-			primaryStage.setTitle("Boardgame");
-			
-			boardVBox = new VBox();
-			
-			boardScene = new Scene(boardVBox,800,800);
-			
-			boardScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-
-			
 			System.out.println("# Beginning of eco main #");
+			
+			this.primaryStage = primaryStage;
+			primaryStage.setTitle("Boardgame - Eco Game - Random Strategy - 2 predefined players - 6 rounds, 10x10 board");
+			
+			mainScreenHBox = new HBox();
+			
+			mainScreenScene = new Scene(mainScreenHBox);
+			
+			mainScreenScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
 			Strategy strat = new RandomStrat();
 			BoardGame board = createEcoBoard();
 			GameEco game = new GameEco(board, 6);
-
 			EcoPlayer philippe = new EcoPlayer("Gandhi", strat);
 			EcoPlayer musclor = new EcoPlayer("Odette", strat);
 			game.addPlayer(philippe);
 			game.addPlayer(musclor);
 			
 			initBoardView(board);
+			initGameIntelVBox(game);
+			initPlayersIntelVBox(game);
 			
 			game.play();
-
-			System.out.println("\n # End of eco main #");
 			
-			
-			primaryStage.setScene(boardScene);
+			primaryStage.setScene(mainScreenScene);
 			primaryStage.setResizable(false);
 			primaryStage.show();
 			primaryStage.centerOnScreen();
+
+			System.out.println("\n # End of eco main #");
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -73,12 +73,11 @@ public class Main extends Application {
 		return board;
 	}
 	
-	/** Pour le moment est appelé après la partie, les Cell sont chargées avec les éventuelles Unit placées dessus.
-	 * Si appelé avant la partie, les informations ne sont pas (encore) mises à jour au fil de la partie.
+	/** Les Cell sont chargées avec les éventuelles Unit placées dessus.
+	 * Les informations sont (a priori) modifiées quand une Unit y est déployée ou en est retirée (ou traitorousUnit()).
 	 * Pour chaque tuile du plateau, on va créer un bouton; chaque bouton forme le plateau sur l'interface. 
 	 * De plus, chaque bouton est cliquable et renvoie sur une autre fenêtre qui donne les informations 
 	 * relatives à la tuile cliquée.
-	 * ?TODO? Implémenter la MAj dynamique des informations contenues dans cette autre fenêtre au fil de la partie.
 	 * ?TODO? Plutôt que de renvoyer vers une nouvelle fenêtre, implémenter les deux fenêtres côte à côte?
 	 * 
 	 * @param board du jeu
@@ -87,8 +86,8 @@ public class Main extends Application {
 		int boardY = board.getWidth();
 		int boardX = board.getLength();
 		for (int y = 0; y < boardY; y++) {
-			HBox buttonsContainer = new HBox();
-			boardVBox.getChildren().add(buttonsContainer);
+			VBox buttonsContainer = new VBox();
+			mainScreenHBox.getChildren().add(buttonsContainer);
 			for (int x = 0; x < boardX; x++) {
 				Cell cell = board.getCell(x, y);
 				VBox cellContentVBox = new VBox(5);
@@ -111,12 +110,11 @@ public class Main extends Application {
 	 */
 	private void initCellScene(Cell cell, Scene cellScene, VBox cellContentVBox) {
 		Label titleLabel= new Label("Cell details");
-		titleLabel.setShape(new Rectangle(10,10));
 		titleLabel.setId("main-title");
 		Button button2= new Button("Back to board");
 		button2.setId("menu-button");
 		button2.setPrefWidth(5000);
-		button2.setOnAction(e -> primaryStage.setScene(boardScene));
+		button2.setOnAction(e -> primaryStage.setScene(mainScreenScene));
 		
 		Label label1 = new Label(cell.getId() + " [" + cell.getX() + ";" + cell.getY() + "]");
 		label1.setId("sub-title");
@@ -132,10 +130,64 @@ public class Main extends Application {
 		cellScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 	}
 	
-	
 	private void updateCellStatus(Cell cell) {
 		cell.updateCellStatus();
 	}
+	
+	private void initGameIntelVBox(Game game) {
+		VBox gameIntelVBox = new VBox();
+		Label titleLabel= new Label("Game intel");
+		titleLabel.setId("main-title");
+		Button button2= new Button("N'appuyez pas sur ce bouton");
+		button2.setId("menu-button");
+		button2.setOnAction(e -> System.out.println("Coucou! Pourquoi tu clique sur ce bouton?"));
+		
+		Label label1 = new Label("This is an Eco Game");
+		label1.setId("sub-title");
+		String availableLabelString = "Available cells : ";
+		for (Cell cell : game.getBoard().getAvailableCells()) {
+			availableLabelString += "\n-"+ cell.toString();
+		}
+		
+		Label label2 = new Label(availableLabelString);
+		Label label3 = new Label(game.getWinner().toString());
+		Label hasBonus = new Label("Honnêtement, jsais pas trop quoi mettre");
+		
+		updateGameStatus(game);
+		
+		gameIntelVBox.getChildren().addAll(button2, titleLabel, label1, label2, label3, hasBonus);
+		mainScreenHBox.getChildren().add(gameIntelVBox);
+	}
+	
+	private void initPlayersIntelVBox(Game game) {
+		VBox playerIntelVBox = new VBox(5);
+		Label titleLabel= new Label("Players intel");
+		titleLabel.setId("main-title");
+		Button button2= new Button("N'appuyez VRAIMENT pas!");
+		button2.setId("menu-button");
+		button2.setOnAction(e -> System.out.println("Mais? Arrête d'appuyer!"));
+		
+		Label label1 = new Label("Gandhi VS Odette");
+		label1.setId("sub-title");
+		String availableLabelString = "Available cells : ";
+		for (Cell cell : game.getBoard().getAvailableCells()) {
+			availableLabelString += "\n-"+ cell.toString();
+		}
+		
+		Label label2 = new Label(availableLabelString);
+		Label label3 = new Label(game.getWinner().toString());
+		Label hasBonus = new Label("Honnêtement, jsais pas trop quoi mettre");
+		
+		updateGameStatus(game);
+		
+		playerIntelVBox.getChildren().addAll(button2, titleLabel, label1, label2, label3, hasBonus);
+		mainScreenHBox.getChildren().add(playerIntelVBox);
+	}
+	
+	private void updateGameStatus(Game game) {
+		//game.updateCellStatus();
+	}
+	
 	/*
 	private class CellButtonHandler implements EventHandler<ActionEvent> {
 		@Override
